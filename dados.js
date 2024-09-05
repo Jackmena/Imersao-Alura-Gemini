1475f1d977a0661cce83aaf3b4c3905841270096
Jackson Mena <mena.jack72@gmail.com>
2024-09-04 22:05:00 -0300

    inciando projeto

---
+++ dados.js
@@ -0,0 +1,20 @@
+let info = [
+  {
+    titulo: "Naruto",
+    descricao:
+      "Naruto (ナルト?) é uma série de mangá escrita e ilustrada por Masashi Kishimoto, que conta a história de Naruto Uzumaki, um jovem ninja que constantemente procura por reconhecimento e sonha em se tornar Hokage, o ninja líder de sua vila.",
+    link: "https://pt.wikipedia.org/wiki/Naruto",
+  },
+  {
+    titulo: "One Piece",
+    descricao:
+      "One Piece (ワンピース Wan Pīsu?) é uma série de mangá escrita e ilustrada por Eiichiro Oda.One Piece conta as aventuras de Monkey D. Luffy, um jovem cujo corpo ganhou as propriedades de borracha após ter comido um fruto do diabo acidentalmente. Com sua tripulação, os Piratas do Chapéu de Palha, Luffy explora a Grand Line em busca do tesouro mais procurado do mundo, o ONE PIECE, a fim de se tornar o próximo Rei dos Piratas.",
+    link: "https://pt.wikipedia.org/wiki/One_Piece",
+  },
+  {
+    titulo: "Gintama",
+    descricao:
+      "Gintama (Alma de Prata), também chamado Gin Tama, é um mangá escrito e ilustrado por Hideaki Sorachi e serializado desde 8 de dezembro de 2003 na revista Weekly Shonen Jump. Situado no Período Edo que foi conquistado por alienígenas chamados Amanto, o enredo segue a vida do ponto de vista do samurai Gintoki Sakata, que trabalha como freelancer ao lado de seus amigos Shimura Shinpachi e Kagura a fim de pagar o aluguel mensal.",
+    link: "https://pt.wikipedia.org/wiki/Gintama",
+  },
+];
